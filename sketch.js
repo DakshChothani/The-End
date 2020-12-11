200e462cbd7e0dcf57b3b1eec78b6976bd3e8e81
DakshChothani <71880714+DakshChothani@users.noreply.github.com>
2020-12-11 21:23:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,127 @@
+var PLAY = 0;
+var END = 1;
+var gameState = PLAY;
+
+var backImage,backgr;
+var player, player_running;
+var ground,ground_img;
+
+var FoodGroup, Coin;
+var obstaclesGroup, obstacle_img;
+
+var score=0;
+
+function preload(){
+  backImage=loadImage("Background.PNG");
+  player_running = loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png");
+  Coin = loadImage("Coin.PNG");
+  obstacle_img = loadImage("stone.png"); 
+  
+}
+
+function setup() {
+  createCanvas(800,400);
+  
+  backgr=createSprite(0,0,800,400);
+  backgr.addImage(backImage);
+  backgr.scale=1.5;
+  backgr.x=backgr.width/2;
+  
+  player = createSprite(100,340,20,50);
+  player.addAnimation("Running",player_running);
+  player.scale = 0.1;
+  
+  ground = createSprite(400,350,800,10);
+
+  ground.x=ground.width/2;
+  ground.visible=false;
+  
+  FoodGroup = new Group();
+  obstaclesGroup = new Group();
+  
+  score = 0;
+}
+
+function draw() {
+  background(255);
+
+    if(gameState === PLAY){
+      if(ground.x<0) {
+        ground.x=ground.width/2;
+      }
+      if(backgr.x<100){
+        backgr.x=backgr.width/2;
+      }
+      
+        if(FoodGroup.isTouching(player)){
+          FoodGroup.destroyEach();
+        score = score + 2;
+        }
+        ground.velocityX=-4;
+        backgr.velocityX=-4;
+      
+        if(keyDown("space") ) {
+          player.velocityY = -12;
+        }
+        player.velocityY = player.velocityY + 0.8;
+        player.collide(ground);
+        spawnFood();
+        spawnObstacles();
+    }
+
+    if(score === 10){
+      gameState = END;
+    }
+
+    if(gameState === END){
+      ground.velocityX = 0;
+      backgr.velocityX=0;
+      spawnFood.visible = false;
+      spawnObstacles.visible = false;
+    }
+ 
+    if(obstaclesGroup.isTouching(player)){ 
+      player.scale=0.08;
+    }
+
+  drawSprites();
+  
+  stroke("white");
+  textSize(20);
+  fill("white");
+  text("Score: "+ score, 500,50);
+
+  if(gameState === END){
+  fill("yellow");
+  textSize(30);
+  text("You Win :)", 350, 200);
+  }
+
+}
+
+function spawnFood() {
+  if (frameCount % 80 === 0) {
+    var banana = createSprite(600,250,40,10);
+    banana.y = random(120,200);    
+    banana.addImage(Coin);
+    banana.scale = 0.6;
+    banana.velocityX = -5;
+    banana.lifetime = 300;
+    player.depth = banana.depth + 1;
+    
+    FoodGroup.add(banana);
+  }
+}
+
+function spawnObstacles() {
+  if(frameCount % 300 === 0) {
+    var obstacle = createSprite(800,300,10,40);
+    obstacle.velocityX = -6;
+    obstacle.addImage(obstacle_img);
+      
+    obstacle.scale = 0.2;
+    obstacle.lifetime = 300;
+    
+    obstaclesGroup.add(obstacle);
+  }
+}
